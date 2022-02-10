747d222ce8bf718c9260dc9bed416a17c89d006c
paget <arnaud.paget@etu.umontpellier.fr>
2022-02-10 14:33:26 +0100

    Initial Commit

---
+++ Shared/fiche_technique_cuisineApp.swift
@@ -0,0 +1,17 @@
+//
+//  fiche_technique_cuisineApp.swift
+//  Shared
+//
+//  Created by m1 on 10/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct fiche_technique_cuisineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
